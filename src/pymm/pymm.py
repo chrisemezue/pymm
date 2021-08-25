f7b648a187253395185f5b610c601d11164a8307
edAI <edai.official.edai@gmail.com>
2021-08-25 18:18:34 +0200

    my tweaks

--- src/pymm/pymm.py
+++ src/pymm/pymm.py
@@ -22,7 +22,7 @@ class MetamapStuck(Exception):
 class Metamap:
     """ MetaMap Concept Extractor """
 
-    def __init__(self, metamap_path, debug=False):
+    def __init__(self, metamap_path,output_file,input_file,use_only_snomed,debug=False):
         """ MetaMap Wrapper parameters
 
         Args:
@@ -31,9 +31,13 @@ class Metamap:
         """
         self.metamap_path = metamap_path
         self.debug = debug
-        self.input_file, self.output_file = self._get_temp_files()
+        self.output_file = output_file
+        self.input_file = input_file
+        self.input_file_, self.output_file_ = self._get_temp_files()
+        self.use_only_snomed=use_only_snomed
+        
         self.metamap_command = MetamapCommand(self.metamap_path,
-                self.input_file, self.output_file, self.debug)
+                self.input_file, self.output_file,self.use_only_snomed, self.debug)
 
     def is_alive(self):
         """Check if MetaMap is running
@@ -44,7 +48,7 @@ class Metamap:
         try:
             mmos = self.parse(["heart attack"])
             concepts = [concept for _, mmo in enumerate(mmos) for _, concept in enumerate(mmo)]
-            return True if len(concepts) == 7 else False
+            return True if len(concepts) > 1 else False
         except ExpatError:
             return False
 
@@ -62,12 +66,12 @@ class Metamap:
             output_file = fp.name
 
         if self.debug:
-            print (f"Input File: {input_file}, Output File: {output_file}")
+            print (f"Input File: {input_file}, Output File: {self.output_file}")
 
         return input_file, output_file
 
 
-    def parse(self, sentences, timeout=10):
+    def parse(self, inp_file, timeout=10):
         """Returns the UMLS concetps
 
         Args:
@@ -77,16 +81,19 @@ class Metamap:
         Returns:
             Iterator over MetaMap Objects
         """
+        '''
         with open(self.input_file, mode="wb") as fp:
             for idx, sentence in enumerate(sentences):
                 fp.write("{0}\n".format(sentence).encode('utf8'))
-
-        try:
-            self.metamap_command.execute(timeout=timeout)
-            return parse(self.output_file)
-        except TimeoutExpired:
-            print ("Execution of command Timedout; try increasing the timeout")
-            raise MetamapStuck()
+        '''
+        self.input_file = inp_file
+        #try:
+        self.metamap_command.execute(inp_file,timeout=timeout)
+        #return parse(self.output_file)
+        return 'All Done'
+        #except TimeoutExpired:
+        #    print ("Execution of command Timedout; try increasing the timeout")
+        #    raise MetamapStuck()
 
 
     def close(self):
